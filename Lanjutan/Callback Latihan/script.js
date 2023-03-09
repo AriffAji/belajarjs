b044b4444fddde9035994f6f1fc0daa0a01a9d03
Arifaji <arifnauval1530@gmail.com>
2023-03-09 21:36:24 +0700

    Belajar Javascript

---
+++ Lanjutan/Callback Latihan/script.js
@@ -0,0 +1,62 @@
+$(".search-button").on("click", function () {
+  $.ajax({
+    url: "https://www.omdbapi.com/?i=tt3896198&apikey=35cada45&s=" + $(".input-keyword").val(),
+    success: (results) => {
+      const movies = results.Search;
+      let cards = "";
+      movies.forEach((m) => {
+        cards += showCards(m);
+      });
+      $(".movie-container").html(cards);
+
+      // Ketika tombol detail di klik
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url: "https://www.omdbapi.com/?apikey=35cada45&i=" + $(this).data("imdbid"),
+          success: (m) => {
+            const movieDetail = showMovieDetail(m);
+            $(".modal-body").html(movieDetail);
+          },
+          error: (e) => {
+            console.log(e.responseText);
+          },
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
+});
+
+function showCards(m) {
+  return `<div class="col-md-3 my-3">
+                  <div class="card">
+                    <img src="${m.Poster}" class="card-img-top" alt="" />
+                    <div class="card-body">
+                      <h5 class="card-title">${m.Title}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                      <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                    </div>
+                  </div>
+                </div>`;
+}
+
+function showMovieDetail(m) {
+  return ` <div class="container-fluid">
+                                  <div class="row">
+                                    <div class="col-md-3">
+                                      <img src="${m.Poster}" class="img-fluid" />
+                                    </div>
+                                    <div class="col-md">
+                                      <ul class="list-group">
+                                        <li class="list-group-item"><h4>${m.Title}(${m.Year})</h4></li>
+                                        <li class="list-group-item"><strong>${m.Director}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Actors}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Writer}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Plot}</strong></li>
+                                      </ul>
+                                    </div>
+                                  </div>
+                                </div>`;
+}
--- Lanjutan/README.md
+++ Lanjutan/README.md
@@ -1,3 +1,3 @@
 # Note untuk sumber
 
-<https://www.youtube.com/watch?v=jRsSVw3Ibhw&list=PLFIM0718LjIUGpY8wmE41W7rTJo_3Y46-&index=23>
+<https://www.youtube.com/watch?v=k1jVq9WGBdM&list=PLFIM0718LjIUGpY8wmE41W7rTJo_3Y46-&index=24>
